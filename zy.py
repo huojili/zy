4b597121058e5703b9e8d9472a52fd57ce7055c4
huojili <111510772+huojili@users.noreply.github.com>
2022-09-25 13:03:23 +0800

    Add files via upload

---
+++ zy.py
@@ -0,0 +1,10 @@
+#sum  是局部变量
+#num1 ，num2是全局变量
+# 15.0和2.6
+def calculate(length1,length2,length3):
+    #length1没有用
+    return length2*length3/2
+
+result = calculate(5,10,6)
+print(result)
+#国际象棋在这个文件下面（上面）
